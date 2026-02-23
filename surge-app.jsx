9ab72e638a3c0ef069827c7a996d5c9964d9427b
imperador7pro-spec <imperador7.pro@gmail.com>
2026-02-23 13:16:32 +0100

    Update Stripe link for paywall CTA

--- surge-app.jsx
+++ surge-app.jsx
@@ -2218,7 +2218,7 @@ function PaywallModal({ book, onClose }) {
         </div>
         <a
           className="paywall-cta"
-          href="https://buy.stripe.com/REMPLACE_PAR_TON_LIEN_STRIPE"
+          href=https://buy.stripe.com/3cI00jcyP6tp7yla0Z6Ri00
           target="_blank"
           rel="noopener noreferrer"
         >
